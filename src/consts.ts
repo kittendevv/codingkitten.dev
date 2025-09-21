2a2efbbf8a9cfc33baed9b1b473b4385b2a5699d
CodingKitten <codingkitten@hackclub.app>
2025-09-21 12:51:20 +0000

    Big ahh commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'CodingKitten';
+export const SITE_DESCRIPTION = 'Hi! I’m CodingKitten, a 15-year-old hobby developer from the Netherlands.';
