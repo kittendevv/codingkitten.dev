1f91c35c7a9d1bfb7e584cc81dcd0074ab0f832c
kittendevv <codingkitten@disroot.org>
2026-04-26 13:51:18 +0200

    Add cloudflare pages adapter, update readme

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
@@ -12,9 +12,7 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		})
+		adapter: adapter()
 	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) => (filename.includes('node_modules') ? undefined : { runes: true })
